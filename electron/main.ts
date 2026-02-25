43daecde9b39e26eee593e098bab102e679215cb
Atrayan <atrayanmukherjee0248@gmail.com>
2026-02-25 10:44:58 +0530

    Initial Commit

---
+++ electron/main.ts
@@ -0,0 +1,29 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let mainWindow: BrowserWindow | null = null;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1600,
+        height: 900,
+        backgroundColor: "#121212",
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+            nodeIntegration: false,
+            contextIsolation: true
+        }
+    });
+
+    mainWindow.loadURL("http://localhost:5173");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
+});
